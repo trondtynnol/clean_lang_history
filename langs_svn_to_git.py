a52cc93d3901e6abe6a20e8da20c73db64bd8671
Børre Gaup <albbas@gmail.com>
2022-10-20 13:24:55 +0200

    Print pre and post logs

--- langs_svn_to_git.py
+++ langs_svn_to_git.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import glob
 import os
-import shutil
 import subprocess
 import sys
 
@@ -9,26 +8,26 @@ import sys
 
 
 def main():
+    if len(sys.argv) != 3:
+        print(
+            "usage: ./langs_svn_to_git.py <dir-where-giellalt-is-clone> <dir-where-work-is-done>"
+        )
+    work_directory = os.path.abspath(sys.argv[2])
+    try:
+        prepare_old_svn(work_directory)
+    except subprocess.CalledProcessError as error:
+        print(error)
+
     git_repos_home = os.path.abspath(
         sys.argv[1]
     )  # run gut clone -o giellalt -r lang-* to populate this directory
-    work_directory = os.path.abspath(sys.argv[2])
-
-    # try:
-    #     prepare_old_svn(work_directory)
-    # except subprocess.CalledProcessError as error:
-    #     print(error)
-    for git_repo_name in ["lang-sme"]:  # get_valid_repo_names(git_repos_home):
-        # if os.path.exists(f"{work_directory}/{git_repo_name}"):
-        #     print(git_repo_name, "has already been processed")
-        # else:
+    for git_repo_name in get_valid_repo_names(git_repos_home):
         try:
             fix_a_lang(git_repo_name, work_directory)
         except subprocess.CalledProcessError as error:
             print(error)
         except IndexError as error:
             print(error)
-    print("main done")
 
 
 def prepare_old_svn(work_directory):
@@ -63,99 +62,158 @@ def fix_a_lang(git_repo_name, work_directory):
         print("No need to process", git_repo_name)
     else:
         svn_lang = git_repo_name.split("-")[1]
-        # prepare_for_rebase(git_repo_name, svn_elements, svn_lang, work_directory)
-        rebase_new_on_old(git_repo_name, svn_lang, work_directory)
-        # cleanup(git_repo_name, svn_lang, work_directory)
+        prepare_for_git_replace(git_repo_name, svn_elements, svn_lang, work_directory)
+        if not os.path.exists(f"{work_directory}/{git_repo_name}"):
+            do_git_replace(git_repo_name, svn_lang, work_directory)
 
 
-def prepare_for_rebase(git_repo_name, svn_directories, svn_lang, work_directory):
-    # The process stops here for some languages because of name change collisions.
+def prepare_for_git_replace(git_repo_name, svn_directories, svn_lang, work_directory):
     # Find the directory where the language last lived in the svn repo by running
     # the git log commands found in svn_lang_dirs function and look at the dates.
-    # Remove all the --path-rename options from command found in the log
-    # in the terminal window, except the one for the directory that was found,
-    # then run that command.
-    # Proceed with running the rest of the commands manually.
-    paths = [f"--path {svn_directory}" for svn_directory in svn_directories]
-    renames = [f"--path-rename {svn_directory}/:" for svn_directory in svn_directories]
+    if not os.path.exists(f"{work_directory}/{svn_lang}-mirror"):
+        paths = [f"--path {svn_directory}" for svn_directory in svn_directories]
+        renames = [
+            f"--path-rename {svn_directory}/:" for svn_directory in svn_directories
+        ]
+        commands = [
+            (f"git clone --mirror --no-local lt {svn_lang}-mirror", work_directory),
+            (
+                f"git filter-repo {' '.join(paths)} {' '.join(renames)}",
+                f"{work_directory}/{svn_lang}-mirror",
+            ),
+        ]
+        for command in commands:
+            run(command[0], cwd=command[1])
+
+
+def do_git_replace(git_repo_name, svn_lang, work_directory):
     commands = [
-        (f"git clone --mirror --no-local lt {svn_lang}-mirror", work_directory),
-        (
-            f"git filter-repo {' '.join(paths)} {' '.join(renames)}",
-            f"{work_directory}/{svn_lang}-mirror",
-        ),
         (f"git clone git@github.com:giellalt/{git_repo_name}", work_directory),
-    ]
-    for command in commands:
-        run(command[0], cwd=command[1])
-
-
-def rebase_new_on_old(git_repo_name, svn_lang, work_directory):
-    # The last commit in the old history
-    old_head = subprocess.run(
-        ["git", "log", "-n", "1", '--pretty=format:"%H %an %ad"'],
-        cwd=f"{work_directory}/{svn_lang}-mirror",
-        encoding="utf-8",
-        capture_output=True,
-    ).stdout[1:-1]
-
-    commit_b = old_head.split()[0]
-    print(f"old_head: {old_head}")
-    print(f"commit_b: {commit_b}")
-
-    # The last commit in current history
-    main_log_lines = subprocess.run(
-        ["git", "log", '--pretty=format:"%H %an %ad"'],
-        cwd=f"{work_directory}/{git_repo_name}",
-        encoding="utf-8",
-        capture_output=True,
-    ).stdout.split("\n")
-    print(f"len of main_log_lines: {len(main_log_lines)}")
-    print(main_log_lines[0])
-    # The github history starts at this commit
-    needle = f"{old_head.replace(commit_b, '')}"
-    first_important_git_commit = [
-        line[1:-1] for line in main_log_lines if needle in line
-    ][0]
-    print(f"first_important: {first_important_git_commit}")
-    commit_a = first_important_git_commit.split()[0]
-    print(commit_a)
-
-    commands = [
         (
             f"git fetch ../{svn_lang}-mirror master:old",
             f"{work_directory}/{git_repo_name}",
         ),
-        (f"git switch -c main_with_history_fixed", f"{work_directory}/{git_repo_name}"),
+    ]
+    for command in commands:
+        run(command[0], cwd=command[1])
+
+    # The last commit in the old history
+
+    old_head = subprocess.run(
+        ["git", "log", "-n", "1", "--pretty=format:%H %an %aI", "old"],
+        cwd=f"{work_directory}/{git_repo_name}",
+        encoding="utf-8",
+        capture_output=True,
+    ).stdout
+
+    commit_b = old_head.split()[0]
+    print(f"commit_b {commit_b}")
+    # The log history of main
+    main_log_lines = subprocess.run(
+        ["git", "log", "--pretty=format:%H %an %aI", "main"],
+        cwd=f"{work_directory}/{git_repo_name}",
+        encoding="utf-8",
+        capture_output=True,
+    ).stdout.split("\n")
+
+    with (open(f"{git_repo_name.split('-')[1]}-pre.log", "w")) as pre_stream:
+        print(
+            subprocess.run(
+                ["git", "log", "--pretty=format:%s %aI", "main"],
+                cwd=f"{work_directory}/{git_repo_name}",
+                encoding="utf-8",
+                capture_output=True,
+            ).stdout,
+            file=pre_stream,
+        )
+    needle = f"{old_head.replace(commit_b, '')}"
+
+    # Find the old commit in main
+    first_important_git_commit = [line for line in main_log_lines if needle in line][0]
+    commit_a = first_important_git_commit.split()[0]
+    print(f"commit_a {commit_a}")
+
+    print(f"git replace {commit_a} {commit_b}")
+
+    commands = [
         (f"git replace {commit_a} {commit_b}", f"{work_directory}/{git_repo_name}"),
-        (f"git filter-repo --force", f"{work_directory}/{git_repo_name}")
-        #     # The process stops at the rebase for some languages because of conflicts
-        #     # If this is the case, then manually
-        #     #   merge incoming changes each time the rebase stops
-        #     #   `git add` files that have been fixed
-        #     #   git rebase --continue (or git rebase --skip for empty commits)
-        #     # the run switch -c command, followed by the ones in cleanup()
-        #     (
-        #         # merge histories, rebase the commits in the range from "first_import_git_commit" to "main_head"
-        #         # onto the old history
-        #         f"git rebase --onto {old_head} {first_important_git_commit}^ {main_head}",
-        #         f"{work_directory}/{git_repo_name}",
-        #     ),
+        (
+            "git filter-repo --replace-refs delete-no-add --force --prune-degenerate always",
+            f"{work_directory}/{git_repo_name}",
+        ),
+    ]
+    for command in commands:
+        run(command[0], cwd=command[1])
+
+    main_with_parents = subprocess.run(
+        ["git", "log", "--pretty=format:%H %P", "main"],
+        cwd=f"{work_directory}/{git_repo_name}",
+        encoding="utf-8",
+        capture_output=True,
+    ).stdout.split("\n")
+
+    children = "\n".join(
+        [
+            f"\t{child}"
+            for main_line in main_with_parents
+            if main_line.endswith(commit_b)
+            for child in main_line.split()[:-1]
+        ]
+    )
+
+    print(f"children of {commit_b}\n{children}")
+    cleanup(git_repo_name, svn_lang, work_directory)
+
+
+def cleanup(git_repo_name, svn_lang, work_directory):
+    commands = [
+        # (f"git branch -D old", f"{work_directory}/{git_repo_name}"),
+        (
+            "git filter-repo --replace-refs delete-no-add --force "
+            "--prune-degenerate always",
+            f"{work_directory}/{git_repo_name}",
+        ),
+        (f"git branch -D main_with_history_fixed", f"{work_directory}/{git_repo_name}"),
+        # (
+        #     f"git push --force -d origin main_with_history_fixed",
+        #     f"{work_directory}/{git_repo_name}",
+        # ),
+    ]
+
+    for command in commands:
+        run(command[0], cwd=command[1])
+
+    with (open(f"{svn_lang}-post.log", "w")) as pre_stream:
+        print(
+            subprocess.run(
+                ["git", "log", "--pretty=format:%s %aI", "main"],
+                cwd=f"{work_directory}/{git_repo_name}",
+                encoding="utf-8",
+                capture_output=True,
+            ).stdout,
+            file=pre_stream,
+        )
+    mailmappath = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), "all-repos.mailmap"
+    )
+    commands = [
+        (
+            f"git filter-repo --replace-refs delete-no-add --force "
+            f"--prune-degenerate always --mailmap {mailmappath}",
+            f"{work_directory}/{git_repo_name}",
+        )
     ]
     for command in commands:
         run(command[0], cwd=command[1])
 
 
-def cleanup(git_repo_name, svn_lang, work_directory):
-    commands = [(f"git branch -D old", f"{work_directory}/{git_repo_name}")]
-    for command in commands:
-        run(command[0], cwd=command[1])
-    shutil.rmtree(f"{work_directory}/{svn_lang}-mirror")
-
-
 def run(command, cwd=""):
-    print(f"\n{command}")
-    subprocess.run(command.split(), cwd=cwd, check=True, encoding="utf-8")
+    if cwd:
+        print(f"cd {cwd}")
+    print(f"{command}\n")
+    subprocess.run(
+        command.split(), cwd=cwd, check=True, encoding="utf-8", capture_output=True
+    )
 
 
 def get_valid_repo_names(git_repos_home):
@@ -227,21 +285,7 @@ if __name__ == "__main__":
 # cd repo
 # git filter-repo --mailmap my-mailmap
 
-
-# Parent rewriting
-
-# To replace $commit_A with $commit_B (e.g. make all commits which had $commit_A as a parent instead have $commit_B for that parent), and rewrite history to make it permanent:
-
-# git replace $commit_A $commit_B
-# git filter-repo --force
-
-# To create a new commit with the same contents as $commit_A except with different parent(s) and then replace $commit_A with the new commit, and rewrite history to make it permanent:
-
-# git replace --graft $commit_A $new_parent_or_parents
-# git filter-repo --force
-
-# The reason to specify --force is two-fold: filter-repo will error out if no arguments are specified, and the new graft commit would otherwise trigger the not-a-fresh-clone check.
-
-# ❯ grep 'Obsolete test' main.txt
-# 239ea63d8 Obsolete test script, removed. Checked with Lene and Trond.
-# dd1681737 Obsolete test script, removed. Checked with Lene and Trond.
+# https://stackoverflow.com/questions/49563574/git-delete-one-of-two-parent-commits, answer two
+# git replace --edit child of commit_a or commit_b
+# in the editor, remove the suspicios parent
+# git filter-repo --replace-refs delete-no-add --force --prune-degenerate always
